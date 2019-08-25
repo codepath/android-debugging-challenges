7d1a00d9335019410d8a1478188198356837b55d
Slava Panasenko <usverster@gmail.com>
2019-08-24 22:13:56 -0700

    Update w/ AndroidX & AsyncHttpClient

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -2,7 +2,7 @@ package com.codepath.debuggingchallenges.activities;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import com.codepath.debuggingchallenges.R;
